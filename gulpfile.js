721b390e5c48e8e36537f63f8175d88a213ba9ed
joaomosantos <maxmillion_kbps@outlook.com>
2015-11-05 11:24:44 -0200

    corrigindo estrutura

--- gulpfile.js
+++ gulpfile.js
@@ -5,32 +5,32 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 
 /* Compilar LESS */
-gulp.task('less', function() {
-  gulp.src('css/less/*.less')
+gulp.task('compiler-less', function() {
+  gulp.src('src/css/less/*.less')
   .pipe(less())
-  .pipe(gulp.dest('css'));
+  .pipe(gulp.dest('src/css'));
 });
 
 /* Concatenar todos os LESS em um único CSS */
-gulp.task('concat-css', ['less'], function() {
-  gulp.src('css/*.css')
+gulp.task('concat-css', ['compiler-less'], function() {
+  gulp.src('src/css/*.css')
   .pipe(concat('all.css'))
-  .pipe(gulp.dest('css'));
+  .pipe(gulp.dest('src/css/final'));
 });
 
 /* Gerar CSS minificado */
 gulp.task('minify-css', ['concat-css'], function() {
-  gulp.src('css/all.css')
+  gulp.src('src/css/final/all.css')
   .pipe(minifyCSS())
   .pipe(rename(function (path) {
     path.basename += '.min'
   }))
-  .pipe(gulp.dest('css'));
+  .pipe(gulp.dest('src/css/final'));
 });
 
 
 gulp.task('watch', function() {
-  gulp.watch('css/less/*.less' , ['less', 'concat-css', 'minify-css']);
+  gulp.watch('src/css/less/*.less' , ['compiler-less']);
 });
 
-gulp.task('default', ['less', 'concat-css', 'minify-css', 'watch']);
+gulp.task('default', ['compiler-less', 'watch']);
