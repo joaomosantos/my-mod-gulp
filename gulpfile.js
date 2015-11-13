6324c7d7d0432932073f9cf4ea6d349babfb6190
joaomosantos <maxmillion_kbps@outlook.com>
2015-11-12 22:16:35 -0200

    alteracao de estrutura gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,61 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
-var minifyCSS = require('gulp-minify-css');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var browserSync = require('browser-sync');
-var ssi = require('browsersync-ssi');
-var autoprefixer = require('gulp-autoprefixer');
-var sass = require('gulp-sass');
+less = require('gulp-less'),
+minifyCSS = require('gulp-minify-css'),
+concat = require('gulp-concat'),
+rename = require('gulp-rename'),
+browserSync = require('browser-sync'),
+ssi = require('browsersync-ssi'),
+autoprefixer = require('gulp-autoprefixer'),
+sass = require('gulp-sass');
+
+var configs = {
+  less: {
+    source: './css/less/*.less',
+    dest: './css/'
+  },
+  sass: {
+    source: './css/sass/*.scss',
+    dest: './css/'
+  },
+  css: {
+    source: './css/*.css',
+    dest: './css/final/',
+    main: 'all.css',
+    root: './css/'
+  },
+  sync: {
+    ext: [
+    '*.html',
+    '*.shtm',
+    '*.shtml',
+    './css/**/*.css'
+    ]
+  }
+};
 
 gulp.task('default', function() {});
 
 /* Compilar LESS */
 gulp.task('compiler-less', function() {
-  gulp.src('css/less/*.less')
+  gulp.src(configs.less.source)
   .pipe(less())
   .pipe(autoprefixer({
     browsers: ['last 2 versions'],
     cascade: false
   }))
-  .pipe(gulp.dest('css'))
+  .pipe(gulp.dest(configs.less.dest))
   .pipe(browserSync.stream());
 });
 
 /* Compilar SASS */
 gulp.task('compiler-sass', function() {
-  gulp.src('css/sass/*.scss')
+  gulp.src(configs.sass.source)
   .pipe(sass())
   .pipe(autoprefixer({
     browsers: ['last 2 versions'],
     cascade: false
   }))
-  .pipe(gulp.dest('css'))
+  .pipe(gulp.dest(configs.sass.dest))
   .pipe(browserSync.stream());
 });
 
@@ -46,35 +71,34 @@ gulp.task('server', function() {
       })
     }
   });
-  gulp.watch('css/less/*.less', ['compiler-less'], browserSync.reload);
-  gulp.watch('css/sass/*.scss', ['compiler-sass'], browserSync.reload);
-  gulp.watch('*.html', browserSync.reload);
-  gulp.watch('*.shtm', browserSync.reload);
+  gulp.watch(configs.less.source, ['compiler-less'], browserSync.reload);
+  gulp.watch(configs.sass.source, ['compiler-sass'], browserSync.reload);
+  gulp.watch(configs.sync.ext, browserSync.reload);
 });
 
 /* Concatenar em um único CSS */
 gulp.task('concat-css', function() {
-  gulp.src('css/*.css')
-  .pipe(concat('all.css'))
-  .pipe(gulp.dest('css/final'));
+  gulp.src(configs.css.source)
+  .pipe(concat(configs.css.main))
+  .pipe(gulp.dest(configs.css.dest));
 });
 
 /* Gerar CSS minificado */
 gulp.task('minify-css', ['concat-css'], function() {
-  gulp.src('css/final/all.css')
+  gulp.src(configs.css.dest + configs.css.main)
   .pipe(minifyCSS())
   .pipe(rename(function (path) {
     path.basename += '.min'
   }))
-  .pipe(gulp.dest('css/final'));
+  .pipe(gulp.dest(configs.css.dest));
 });
 
 // Adicionar auto prefixo
 gulp.task('autoprefixer-css', function() {
-  gulp.src('css/*.css')
+  gulp.src(configs.css.source)
   .pipe(autoprefixer({
     browsers: ['last 2 versions'],
     cascade: false
   }))
-  .pipe(gulp.dest('css'));
+  .pipe(gulp.dest(configs.css.root));
 });
